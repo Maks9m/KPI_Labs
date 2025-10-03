ce2a0a4587c62c6479aea07c0f8bf4696bed3622
Maks9m <maxim.kramarenko8a@gmail.com>
2025-10-03 14:31:22 +0300

    feat(Lab1): added foreign keys to classes

--- Lab1/schema.ts
+++ Lab1/schema.ts
@@ -16,7 +16,8 @@ class Car {
     public fuel: string,
     public price: number,
     public status: string,
-    public book_user: User | null = null
+    public is_in: CarLocation,
+    public booked_by: User | null = null
   ) {}
 }
 
@@ -30,10 +31,12 @@ class Trip {
 
   constructor(
     public trip_id: number,
-    public car_id: Car,
-    public user_id: User,
     public start_time: Date,
     public end_time: Date,
+    public start_location: CarLocation,
+    public end_location: CarLocation,
+    public car: Car,
+    public user: User,
   ) {
     const diffMs = this.end_time.getTime() - this.start_time.getTime();
     const diffMins = Math.floor(diffMs / (1000 * 60));
@@ -42,16 +45,16 @@ class Trip {
     
     this.duration = `${hours}h ${minutes}m`;
 
-    this.cost = this.car_id.price * diffMins;
+    this.cost = this.car.price * diffMins;
   }
 }
 
 class Booking {
   constructor(
     public book_id: number,
-    public user_id: User,
-    public car_id: Car,
-    public status: string
+    public status: string,
+    public user: User,
+    public car: Car,
   ) {}
 }
 
@@ -63,6 +66,7 @@ class Payment {
     public type: string,
     public method: string,
     public status: string,
-    public trip_id: Trip
+    public trip: Trip,
+    public paid_by: User
   ) {}
 }
