a86fe98bec012950953285d623fa4c7ac7e69a3b
edw4rdkk <eduardkyrychenko923@gmail.com>
2025-10-04 11:17:56 +0300

    fix(Lab1): fix entity types

--- Lab1/mermaid.md
+++ Lab1/mermaid.md
@@ -2,52 +2,52 @@
 erDiagram
 direction LR
 USER {
-int user_id PK
+string user_id PK
 string email
 string firstname
 string lastname
 string driver_license
 }
 CAR {
-int car_id PK
+string car_id PK
 string license_plate
 string car_type
 string fuel
 float price
 string status
-int booked_by FK
-int is_in FK
+string booked_by FK
+string is_in FK
 }
 LOCATION {
-int location_id PK
+string location_id PK
 string address
 }
 TRIP {
-int trip_id PK
-int car_id FK
-int car_price FK
-int user_id FK
+string trip_id PK
+string car_id FK
+string car_price FK
+string user_id FK
 datetime start_time
 datetime end_time
 string duration
 decimal cost
-int start_location FK
-int end_location FK
+string start_location FK
+string end_location FK
 }
 BOOKING {
-int book_id PK
-int user_id FK
-int car_id FK
+string book_id PK
+string user_id FK
+string car_id FK
 string status
 }
 PAYMENT {
-int payment_id PK
+string payment_id PK
 date date
 decimal amount
 string type
 string method
 string status
-int trip_id FK
+string trip_id FK
 }
 
     USER||--o{BOOKING:"makes"
@@ -66,52 +66,52 @@ int trip_id FK
 erDiagram
 direction LR
 USER {
-int user_id PK
+string user_id PK
 string email
 string firstname
 string lastname
 string driver_license
 }
 CAR {
-int car_id PK
+string car_id PK
 string license_plate
 string car_type
 string fuel
 float price
 string status
-int booked_by FK
-int is_in FK
+string booked_by FK
+string is_in FK
 }
 LOCATION {
-int location_id PK
+string location_id PK
 string address
 }
 TRIP {
-int trip_id PK
-int car_id FK
-int car_price FK
-int user_id FK
+string trip_id PK
+string car_id FK
+string car_price FK
+string user_id FK
 datetime start_time
 datetime end_time
 string duration
 decimal cost
-int start_location FK
-int end_location FK
+string start_location FK
+string end_location FK
 }
 BOOKING {
-int book_id PK
-int user_id FK
-int car_id FK
+string book_id PK
+string user_id FK
+string car_id FK
 string status
 }
 PAYMENT {
-int payment_id PK
+string payment_id PK
 date date
 decimal amount
 string type
 string method
 string status
-int trip_id FK
+string trip_id FK
 }
 
     USER||--o{BOOKING:"makes"
--- Lab1/schema.ts
+++ Lab1/schema.ts
@@ -1,6 +1,6 @@
 class User {
   constructor(
-    public user_id: number,
+    public user_id: string,
     public email: string,
     public first_name: string,
     public last_name: string,
@@ -10,19 +10,19 @@ class User {
 
 class Car {
   constructor(
-    public car_id: number,
+    public car_id: string,
     public license_plate: string,
     public car_type: string,
     public fuel: string,
     public price: number,
     public status: string,
-    public is_in: CarLocation,
-    public booked_by: User | null = null
+    public is_in: CarLocation["location_id"],
+    public booked_by: User["user_id"] | null = null
   ) {}
 }
 
 class CarLocation {
-  constructor(public location_id: number, public address: string) {}
+  constructor(public location_id: string, public address: string) {}
 }
 
 class Trip {
@@ -30,14 +30,14 @@ class Trip {
   public cost: number;
 
   constructor(
-    public trip_id: number,
+    public trip_id: string,
     public start_time: Date,
     public end_time: Date,
-    public start_location: CarLocation,
-    public end_location: CarLocation,
-    public car_id: Car,
-    public car_price: Car['price'],
-    public user_id: User,
+    public start_location: CarLocation["location_id"],
+    public end_location: CarLocation["location_id"],
+    public car_id: Car["car_id"],
+    public car_price: Car["price"],
+    public user_id: User["user_id"]
   ) {
     const milliseconds = this.end_time.getTime() - this.start_time.getTime();
     const minutes = Math.ceil(milliseconds / (1000 * 60));
@@ -51,22 +51,22 @@ class Trip {
 
 class Booking {
   constructor(
-    public book_id: number,
+    public book_id: string,
     public status: string,
-    public user: User,
-    public car: Car,
+    public user_id: User["user_id"],
+    public car_id: Car["car_id"]
   ) {}
 }
 
 class Payment {
   constructor(
-    public payment_id: number,
+    public payment_id: string,
     public date: Date,
     public amount: number,
     public type: string,
     public method: string,
     public status: string,
-    public trip: Trip,
-    public paid_by: User
+    public trip: Trip["trip_id"],
+    public paid_by: User["user_id"]
   ) {}
 }
